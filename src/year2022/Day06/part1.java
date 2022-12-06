06510b9970fcd8848aaf325ff07b6564e7cc2678
Olesya Naibova <o.naibova@gmail.com>
2022-12-06 11:33:01 -0600

    Solve day 6

---
+++ src/year2022/Day06/part1.java
@@ -0,0 +1,31 @@
+package year2022.Day06;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class part1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("src/year2022/Day06/input.txt"));
+        String line = scanner.nextLine();
+
+        int left = 0;
+        int right = 4;
+        while (right <= line.length()) {
+            if (areCharactersUnique(line.substring(left, right))) {
+                System.out.printf("Need to process %d characters", right);
+                break;
+            }
+            left++;
+            right++;
+        }
+    }
+
+    private static boolean areCharactersUnique(String s) {
+        return s.chars()
+                .filter(e -> Collections.frequency(s.chars().boxed().collect(Collectors.toList()), e) > 1)
+                .count() <= 1;
+    }
+}
